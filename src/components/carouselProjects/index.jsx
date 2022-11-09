41244070397c680bf37c58f99353fda0bf8f6327
DiFernando11 <102831050+DiFernando11@users.noreply.github.com>
2022-11-09 14:31:54 -0500

    responsive/section/projects

--- src/components/carouselProjects/index.jsx
+++ src/components/carouselProjects/index.jsx
@@ -83,85 +83,103 @@ function CarouselProjects({ renderProyects }) {
   };
   return (
     <section className="section_projects">
-      <div className="wrap">
-        <button className="carrusel_button" onClick={prevCardFavority}>
-          <i className="bi bi-chevron-left"></i>
-        </button>
-      </div>
-      <div className="container_section_projectsr_img_video_projects">
-        {projectsPersonal.length &&
-          projectsPersonal.map((project, index) => (
-            <div key={index}>
-              {cardFavoriteCurrent === index && (
-                <div key={index} className="container_img_video_projects">
-                  {!renderProyects ? (
-                    <div className="container_image_preview">
-                      <img
-                        className="video_image_projects_mobile"
-                        src={project.project}
-                      />
-                      <div className="text_img_preview">
-                        <div className="border_box_animation">
-                          <ul>
-                            <li
-                              className="container_tecnologies"
-                              id={project.name}
-                            >
-                              {project.technologies.length
-                                ? project.technologies.map(
-                                    (technology, index) => (
-                                      <img
-                                        key={index}
-                                        src={technology}
-                                        alt={project.name}
-                                      />
+      <div className="flex_container_section_projects">
+        <div className="wrap">
+          <button className="carrusel_button" onClick={prevCardFavority}>
+            <i className="bi bi-chevron-left"></i>
+          </button>
+        </div>
+        <div className="container_section_projectsr_img_video_projects">
+          {projectsPersonal.length &&
+            projectsPersonal.map((project, index) => (
+              <div key={index}>
+                {cardFavoriteCurrent === index && (
+                  <div key={index} className="container_img_video_projects">
+                    {!renderProyects ? (
+                      <div className="container_image_preview">
+                        <img
+                          className="video_image_projects_mobile"
+                          src={project.project}
+                        />
+                        <div className="text_img_preview">
+                          <div className="border_box_animation">
+                            <ul>
+                              <li
+                                className="container_tecnologies"
+                                id={project.name}
+                              >
+                                {project.technologies.length
+                                  ? project.technologies.map(
+                                      (technology, index) => (
+                                        <img
+                                          key={index}
+                                          src={technology}
+                                          alt={project.name}
+                                        />
+                                      )
                                     )
-                                  )
-                                : null}
-                            </li>
-                            <div className="container_flex_previewIcons">
-                              <li>
-                                <h4>{project.name}</h4>
+                                  : null}
                               </li>
-                              <div className="flex_icon_mobile">
+                              <div className="container_flex_previewIcons">
                                 <li>
-                                  <a href={project.aplication} target="_blank">
-                                    <div className="icon_preview">
-                                      <i className="bi bi-eye"></i>
-                                    </div>
-                                  </a>
+                                  <h4>{project.name}</h4>
                                 </li>
-                                <li>
-                                  <a href={project.gitHub} target="_blank">
-                                    <div className="icon_preview">
-                                      <i className="bi bi-github"></i>
-                                    </div>
-                                  </a>
-                                </li>
+                                <div className="flex_icon_mobile">
+                                  <li>
+                                    <a
+                                      href={project.aplication}
+                                      target="_blank"
+                                    >
+                                      <div className="icon_preview">
+                                        <i className="bi bi-eye"></i>
+                                      </div>
+                                    </a>
+                                  </li>
+                                  <li>
+                                    <a href={project.gitHub} target="_blank">
+                                      <div className="icon_preview">
+                                        <i className="bi bi-github"></i>
+                                      </div>
+                                    </a>
+                                  </li>
+                                </div>
                               </div>
-                            </div>
-                          </ul>
+                            </ul>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  ) : (
-                    <video
-                      className="video_image_projects_mobile"
-                      autoPlay
-                      controls
-                    >
-                      <source src={project.video} type="video/mp4" />
-                    </video>
-                  )}
-                </div>
-              )}
-            </div>
-          ))}
+                    ) : (
+                      <video
+                        className="video_image_projects_mobile"
+                        autoPlay
+                        controls
+                      >
+                        <source src={project.video} type="video/mp4" />
+                      </video>
+                    )}
+                  </div>
+                )}
+              </div>
+            ))}
+        </div>
+        <div className="wrap">
+          <button className="carrusel_button" onClick={nextCardFavority}>
+            <i className="bi bi-chevron-right"></i>
+          </button>
+        </div>
       </div>
-      <div className="wrap">
-        <button className="carrusel_button" onClick={nextCardFavority}>
-          <i className="bi bi-chevron-right"></i>
-        </button>
+
+      <div className="container_button_mobileCarrouselProjects">
+        <div className="wrap_mobile">
+          <button className="carrusel_button" onClick={prevCardFavority}>
+            <i className="bi bi-chevron-left"></i>
+          </button>
+        </div>
+        <div className="wrap_mobile">
+          <button className="carrusel_button" onClick={nextCardFavority}>
+            <i className="bi bi-chevron-right"></i>
+          </button>
+        </div>
       </div>
     </section>
   );
